e1a57846f29a1210dee7f2bc82287592d427313b
Aleksandra Korolczuk <korolczulaleksandra@gmail.com>
2017-08-02 20:03:50 -0700

    Homework

---
+++ aug_2.c
@@ -0,0 +1,102 @@
+/*
+///////////////////////////////////////
+// Exercise 2.2 page 42
+//didnt rewrite whole code, just the loop:
+
+#include <stdio.h>
+
+int main() {
+
+for (i=0; i<lim-1; i++){
+if (c=hetchar()) !='\n\){
+if (c != EOF){
+s[i]=c; 
+        }
+        }
+        }
+
+return 0;
+}
+
+EX 2.3 page 48
+int htoi(char s[]){
+
+int i,number, digit;
+
+
+
+for (i = 0;s[0] == '0' && (s[1] == 'x' || s[1] == 'X'); ++i){
+
+if ( s[i] >='0' && s[i] <= '9')
+
+digit = s[i] - '0';
+
+else if(s[i] >='a' && s[i] <= 'f')
+
+digit = s[i] - 'a'+10
+
+else ( s[i] >='A'&& s[i] <= 'F')
+
+digit = s[i] - 'A' +10;
+
+)
+
+n=16 * n + (digit);
+
+return number;
+
+Ex 2.4 page 48
+
+#include <stdio.h>
+char s1[], s2[];
+
+void squeeze(char s1[], char s2[]){
+
+int i,j,k;
+
+for(i=j=0; s1[i] !='\0' ;i++){
+
+if(s1[i] != s2[i]){
+
+s1[j++] = s1[i];
+
+s1[j]='\0';
+
+}
+}
+
+}
+
+EX 2_5 page 48
+
+void any(char s1[], char s2[]){
+
+int i,j;
+
+for(i=j=0; s1[i] !='\0' && s2[j] != '\0';i++){
+if(s1[i] == s2[j]){
+return s1[i];
+else
+;
+}
+
+
+}
+}
+
+}
+
+
+*/
+
+
+int main(){
+
+
+return 0;
+
+}
+
+
+
+
