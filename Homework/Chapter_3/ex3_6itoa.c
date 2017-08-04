c8713a8adab4ec6567804d23de265b7233600f74
Aleksandra Korolczuk <korolczulaleksandra@gmail.com>
2017-08-03 19:55:17 -0700

    Homework 3.6

---
+++ Homework/Chapter_3/ex3_6itoa.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+
+return 0;
+
+}
+
+
+void itoa(int n, char s[], int w){
+int i, sign;
+if((sign=n)<0)
+n=-n;
+i=0;
+
+do {
+
+s[i++]=(n%10)+ '0';
+}while ((n/=10)>0);
+if (sign<0)
+s[i++]='-';
+while (i<w)
+s[i++]=' ';
+
+s[i]='\0';
+
+//reverse(s);
+
+
+
+
+
+}
